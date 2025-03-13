fce4f21874ecc6aca03e834bd8d1d3c18d7f56e6
Mark Meany <mark.meany@gmail.com>
2025-03-13 14:39:15 +0000

    Initial commit

---
+++ api/src/main/java/net/mmeany/play/app/SpringbootMultiModuleStarterApplication.java
@@ -0,0 +1,13 @@
+package net.mmeany.play.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultiModuleStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultiModuleStarterApplication.class, args);
+    }
+
+}
